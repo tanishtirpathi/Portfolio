3da8886ca6d0d15c5684ce173c017860f97e3a01
tanish tirpathi <tanishtirpathi0@gmail.com>
2025-04-30 17:17:05 +0530

    adding the single file

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from "./nav"
+import BodyImageside from './BodyImageside'
+function Main() {
+  return (
+    <>
+    <Nav/>
+   <BodyImageside/>
+    </>
+  )
+}
+
+export default Main
